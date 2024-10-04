127fb2515c0d25532d576540c46c45cfd3d0312b
lightzane <jp.eaguilar@gmail.com>
2024-10-04 16:32:22 +0800

    update

--- src/data/registered_events.ts
+++ src/data/registered_events.ts
@@ -7,29 +7,29 @@ import { uuid } from '../shared/utils';
 
 export const REGISTERED_EVENTS: Event[] = [
   {
-    name: 'Latest Event is here',
-    start: +new Date('04-OCT-2024'),
-    end: +new Date('04-OCT-2024'),
-    time: {
-      start: '12am PDT',
-      end: '11:59pm PDT',
+    "name": "Latest Event is here",
+    "start": +new Date('04-OCT-2024'),
+    "end": +new Date('04-OCT-2024'),
+    "time": {
+      "start": "12am PDT",
+      "end": "11:59pm PDT"
     },
-    id: uuid(),
+    "id": uuid()
   },
   {
-    name: 'Sample Event',
-    start: +new Date('04-OCT-2024'),
-    end: +new Date('04-OCT-2024'),
-    time: {
-      start: '12am PDT',
-      end: '11:59pm PDT',
+    "name": "Sample Event",
+    "start": +new Date('04-OCT-2024'),
+    "end": +new Date('04-OCT-2024'),
+    "time": {
+      "start": "12am PDT",
+      "end": "11:59pm PDT"
     },
-    id: uuid(),
+    "id": uuid()
   },
   {
-    name: 'Another event is here',
-    start: +new Date('04-OCT-2024'),
-    end: +new Date('04-OCT-2024'),
-    id: uuid(),
-  },
+    "name": "Another event is here",
+    "start": +new Date('04-OCT-2024'),
+    "end": +new Date('04-OCT-2024'),
+    "id": uuid()
+  }
 ];
